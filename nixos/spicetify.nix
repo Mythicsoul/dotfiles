22b05ed11540c2e6805b7fff897bf9efec6be5a5
Mythicsoul <mythicsoul.dev@proton.me>
2024-09-09 16:16:30 +0200

    change spicetify theme & add custom color scheme

--- nixos/spicetify.nix
+++ nixos/spicetify.nix
@@ -7,9 +7,22 @@ in
 
   programs.spicetify =  {
     enable = true;
-    theme = spicePkgs.themes.sleek;
-    colorScheme = "rosepine";
+    theme = spicePkgs.themes.lucid; # fix browse icon
+    colorScheme = "custom";
 
+    customColorScheme = {
+      text = "f1f1f1";
+      subtext = "c1c1c1";
+      main = "1a0e26";
+      sidebar = "241c2c";
+      accent = "f9b1e6";
+      player = "241c2c";
+      card = "241c2c";
+      shadow = "241c2c";
+      button = "ff369b";
+      button-active = "ff369b";
+    };
+    
     enabledExtensions = with spicePkgs.extensions; [
       goToSong
       songStats
